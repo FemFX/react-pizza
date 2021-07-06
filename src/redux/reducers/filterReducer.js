b2fd27a22efbb88ca96f03c00eec0e9accbc06bd
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-07-06 11:47:14 +0300

    Create filterReducer.js

---
+++ src/redux/reducers/filterReducer.js
@@ -0,0 +1,20 @@
+import { SET_CATEGORY, SET_SORT_BY } from "../types";
+
+const initialState = {
+  category: null,
+  sortBy: {
+    type: "popular",
+    order: "desc",
+  },
+};
+const filters = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_SORT_BY:
+      return { ...state, sortBy: action.payload };
+    case SET_CATEGORY:
+      return { ...state, category: action.payload };
+    default:
+      return state;
+  }
+};
+export default filters;
