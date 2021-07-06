9893646e37dbd01b8607d26fa3c1f0629fdbbb43
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-07-06 11:44:55 +0300

    compare all importes

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Button } from "./Button";
+export { default as Header } from "./Header";
+export { default as Categories } from "./Categories";
+export { default as SortPopup } from "./SortPopup";
+export { default as CartItem } from "./CartItem";
+export { default as EmptyCart } from "./EmptyCart";
+export { default as PizzaBlock } from "./PizzaBlock";
+export { default as LoadingBlock } from "./PizzaBlock/LoadingBlock";
