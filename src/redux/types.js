822cfc9e18fe6f1ad775bf9bb1acba5e55abf6ba
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-07-06 11:45:11 +0300

    Create types.js

---
+++ src/redux/types.js
@@ -0,0 +1,9 @@
+export const SET_SORT_BY = "SET_SORT_BY";
+export const SET_CATEGORY = "SET_CATEGORY";
+export const SET_PIZZAS = "SET_PIZZAS";
+export const SET_LOADED = "SET_LOADED";
+export const ADD_PIZZA_TO_CART = "ADD_PIZZA_TO_CART";
+export const CLEAR_CART = "CLEAR_CART";
+export const MINUS_CART_ITEM = "MINUS_CART_ITEM"
+export const PLUS_CART_ITEM = "PLUS_CART_ITEM"
+export const REMOVE_CART_ITEM = "REMOVE_CART_ITEM"
