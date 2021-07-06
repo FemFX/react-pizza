1cd532a7ba97345662f08ab20301f11d10b8cd16
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-07-06 11:47:12 +0300

    Create pizzaReducer.js

---
+++ src/redux/reducers/pizzaReducer.js
@@ -0,0 +1,16 @@
+import { SET_LOADED, SET_PIZZAS } from "../types";
+const initialState = {
+  items: [],
+  isLoaded: false,
+};
+const pizzas = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_PIZZAS:
+      return { ...state, items: action.payload, isLoaded: true };
+    case SET_LOADED:
+      return {...state, isLoaded : action.payload}
+    default:
+      return state;
+  }
+};
+export default pizzas;
